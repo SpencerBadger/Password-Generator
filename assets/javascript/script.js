346ac591865cd39155421512f88da57754fdf346
Spencer Badger <spbadger@hotmail.com>
2021-06-24 09:32:04 +1000

    Updated as requested.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,112 +1,116 @@
-// Assignment Code
+// DOM Manipulation to get button from ID 
 var generateBtn = document.querySelector("#generate");
-//Validate the user input
-function getPasswordOptions(userNumCharacters) {
-  if (isNaN(userNumCharacters)) {
-    alert("Please enter a valid number.");
-    return false;
-  } else if (parseInt(userNumCharacters) < 8) {
-    alert("Password length must be at least 8 characters.");
-    return false;
-  } else if (parseInt(userNumCharacters) >= 128) {
-    alert("Password must be less than 129 characters.");
-    return false;
-  }
-  return true;
-}
-//Select random characters for the specific requirement sets.
-function getRandomElementFromArray(collection) {
-  return collection[Math.floor(Math.random() * collection.length)];
-}
-//Prompt user for their requirements.
-function generatePassword() {
-  var userNumCharacters = prompt(
-    "How many characters do you want your password to contain?"
-  );
-  var validPWord = getPasswordOptions(userNumCharacters);
-  if  (validPWord) {
-    var hasSpecial = confirm("Click OK if you require special characters. e.g. ! $ #");
-    var hasNumbers = confirm("Click OK if you wish to include numbers.");
-    var hasLower = confirm("Click OK if you wish to include lower case letters.");
-    var hasUpper = confirm("Click OK if you wish to include capital letters.");
-  }
-//Check if user doesn't include any types of characters. Script will end upon all false values.
-  if (
-    [hasSpecial, hasNumbers, hasLower, hasUpper].includes(
-      true
-    )
-  )
-//Array to store types of characters to include in password
-    var chosenChar = [];
-
-//Array to contain one of each type of chosen character to ensure each will be used
-  var guaranteedChar = [];
-
-//Conditional statements that add array of each type of character into array of possible characters based on user input and pushes new random character to guaranteedCharacters.
-  if (hasSpecial) {
-    chosenChar = chosenChar.concat(specialCharacters);
-    guaranteedChar.push(
-      specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-    );
-  }
-  if (hasNumbers) {
-    chosenChar = chosenChar.concat(numericCharacters);
-    guaranteedChar.push(
-      numericCharacters[Math.floor(Math.random() * numericCharacters.length)]
-    );
-  }
-  if  (hasLower) {
-    chosenChar = chosenChar.concat(lowerCasedCharacters);
-    guaranteedChar.push(
-      lowerCasedCharacters[
-        Math.floor(Math.random() * lowerCasedCharacters.length)
-      ]
-    );
-  }
-  if (hasUpper) {
-    chosenChar = chosenChar.concat(upperCasedCharacters);
-    guaranteedChar.push(
-      upperCasedCharacters[
-        Math.floor(Math.random() * upperCasedCharacters.length)
-      ]
-    );
-  }
-
-//A for loop to iterate the password length from the options object whilst
-// the random characters.
-  var randomChar = [];
-  for (var i = 0; i < userNumCharacters; i++) {
-    var index = Math.floor(Math.random() * chosenChar.length);
-    randomChar.push(chosenChar[index]);
-  }
-  var replacedPosition = {};
-//While loop to ensure a least one of each character type.
-  while (guaranteedChar.length > 0) {
-    var replaceChar = Math.floor(Math.random() * randomChar.length);
-    if (!replacedPosition[replaceChar]) {
-      randomChar[replaceChar] = guaranteedChar.pop();
-      replacedPosition[replaceChar] = true;
-    }
-  }
-  return randomChar.join("");
-}
-
-//Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-//Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 //The array of special characters.
-var specialCharacters = ["@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","}","{","]","[","~","-","_",".",];
+var specialChar = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".", ];
 //The array of numeric characters.
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 //The array of lower case letters.
-var lowerCasedCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
 // The array of upper case letters.
-var upperCasedCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+
+//Select random characters for the specific requirement sets.
+const Random = (collection) => {
+    return collection[Math.floor(Math.random() * collection.length)];
+};
+
+//Validating the user input
+const passwordOptions = (numChar) => {
+    if (isNaN(numChar)) {
+        alert("Please enter a valid number.");
+        return false;
+    } else if (parseInt(numChar) < 8) {
+        alert("Password must be a minimum of 8 characters long.");
+        return false;
+    } else if (parseInt(numChar) >= 128) {
+        alert("Password must be a maximum of 128 characters or less.");
+        return false;
+    }
+    return true;
+
+};
+
+//Prompt user for their requirements.
+const genPassword = () => {
+    var numChar = prompt(
+        "How many characters do you want your password to contain?"
+    );
+    var pWordChecker = passwordOptions(numChar);
+    if (pWordChecker) {
+        var special = confirm("Click OK if you require special characters. e.g. ! $ #");
+        var numbers = confirm("Click OK if you wish to include numbers.");
+        var lower = confirm("Click OK if you wish to include lower case letters.");
+        var upper = confirm("Click OK if you wish to include capital letters.");
+    }
+    //Check if user doesn't include any types of characters. Script will end upon all false values.
+    if (
+        [special, numbers, lower, upper].includes(
+            true
+        )
+    )
+    //Array to store types of characters to include in password
+        var chosenChar = [];
+
+    //Array to contain one of each type of chosen character to ensure each will be used
+    var forcedChar = [];
+
+    //Conditional statements that add array of each type of character into array of possible characters based on user input and pushes new random character to forcedCharacters.
+
+    if (special) {
+        chosenChar = chosenChar.concat(specialChar);
+        forcedChar.push(
+            specialChar[Math.floor(Math.random() * specialChar.length)]
+        );
+    }
+
+    if (numbers) {
+        chosenChar = chosenChar.concat(numericChar);
+        forcedChar.push(
+            numericChar[Math.floor(Math.random() * numericChar.length)]
+        );
+    }
+
+    if (lower) {
+        chosenChar = chosenChar.concat(lowerChar);
+        forcedChar.push(
+            lowerChar[
+                Math.floor(Math.random() * lowerChar.length)
+            ]
+        );
+    }
+
+    if (upper) {
+        chosenChar = chosenChar.concat(upperChar);
+        forcedChar.push(
+            upperChar[
+                Math.floor(Math.random() * upperChar.length)
+            ]
+        );
+    };
+    var randomChar = [];
+    for (var i = 0; i < numChar; i++) {
+        var index = Math.floor(Math.random() * chosenChar.length);
+        randomChar.push(chosenChar[index]);
+    }
+    var replacedPosition = {};
+    //While loop to ensure a least one of each character type.
+    while (forcedChar.length > 0) {
+        var charReplace = Math.floor(Math.random() * randomChar.length);
+        if (!replacedPosition[charReplace]) {
+            randomChar[charReplace] = forcedChar.pop();
+            replacedPosition[charReplace] = true;
+        }
+    }
+    return randomChar.join("");
+};
+
+//Write password to the #password id.
+const displayPassword = () => {
+    var password = genPassword();
+    var textPassword = document.querySelector("#password");
+    textPassword.value = password;
+};
+
+//Add event listener to generate id, so when clicked will display the password.
+generateBtn.addEventListener("click", displayPassword);
